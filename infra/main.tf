eec90d51e0752492d2e9d3688bccd58f350ec993
codeacademy-hw-karan <karan.shirish.karnik@codeacademylt.onmicrosoft.com>
2025-01-06 20:45:28 +0100

    Update main.tf to add output

--- infra/main.tf
+++ infra/main.tf
@@ -13,4 +13,7 @@ resource "aws_instance" "example" {
     Name = "ExampleInstance"
   }
 }
-
+output "instance_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
