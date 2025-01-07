09358e1a0f701b94882de74056346562106fc767
albert <albert.mackel@gmail.com>
2025-01-07 21:52:51 +0200

    changed main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "https" {
   }
 
 ingress {
-    from_port   = 22
+    from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
